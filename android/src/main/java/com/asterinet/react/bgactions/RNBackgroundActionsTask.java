fef037ffcc2ee28c170915216b5f3cc5cda65f03
max.wang <mstar0223@gmail.com>
2024-03-21 16:43:39 +0800

    Initial commit

---
+++ android/src/main/java/com/asterinet/react/bgactions/RNBackgroundActionsTask.java
@@ -0,0 +1,153 @@
+package com.asterinet.react.bgactions;
+
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+import com.facebook.react.HeadlessJsTaskService;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.jstasks.HeadlessJsTaskConfig;
+
+final public class RNBackgroundActionsTask extends HeadlessJsTaskService {
+
+    public static final int SERVICE_NOTIFICATION_ID = 92901;
+    private static final String CHANNEL_ID = "RN_BACKGROUND_ACTIONS_CHANNEL";
+    private @Nullable static BackgroundTaskOptions currentBgOptions;
+
+    @SuppressLint("UnspecifiedImmutableFlag")
+    @NonNull
+    public static Notification buildNotification(@NonNull Context context, @NonNull final BackgroundTaskOptions bgOptions) {
+        currentBgOptions = bgOptions;
+
+        // Get info
+        final String taskTitle = bgOptions.getTaskTitle();
+        final String taskDesc = bgOptions.getTaskDesc();
+        final int iconInt = bgOptions.getIconInt();
+        final int color = bgOptions.getColor();
+        final String linkingURI = bgOptions.getLinkingURI();
+        Intent notificationIntent;
+        if (linkingURI != null) {
+            notificationIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(linkingURI));
+        } else {
+            //as RN works on single activity architecture - we don't need to find current activity on behalf of react context
+            notificationIntent = new Intent(Intent.ACTION_MAIN).addCategory(Intent.CATEGORY_LAUNCHER);
+        }
+        final PendingIntent contentIntent;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_MUTABLE);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+        } else {
+            contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        }
+        final NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setContentTitle(taskTitle)
+                .setContentText(taskDesc)
+                .setSmallIcon(iconInt)
+                .setContentIntent(contentIntent)
+                .setOngoing(bgOptions.isOngoing())
+                .setAutoCancel(bgOptions.isAutoCancel())
+                .setPriority(bgOptions.getPriority())
+                .setColor(color);
+
+        final Bundle progressBarBundle = bgOptions.getProgressBar();
+        if (progressBarBundle != null) {
+            final int progressMax = (int) Math.floor(progressBarBundle.getDouble("max"));
+            final int progressCurrent = (int) Math.floor(progressBarBundle.getDouble("value"));
+            final boolean progressIndeterminate = progressBarBundle.getBoolean("indeterminate");
+            builder.setProgress(progressMax, progressCurrent, progressIndeterminate);
+        }
+        return builder.build();
+    }
+
+    @Nullable
+    private ReactContext getReactContext() {
+        ReactNativeHost reactNativeHost = getReactNativeHost();
+        ReactInstanceManager reactInstanceManager = reactNativeHost.getReactInstanceManager();
+        return reactInstanceManager.getCurrentReactContext();
+    }
+
+    @Override
+    protected @Nullable
+    HeadlessJsTaskConfig getTaskConfig(Intent intent) {
+        final Bundle extras = intent.getExtras();
+        if (extras != null) {
+            return new HeadlessJsTaskConfig(extras.getString("taskName"), Arguments.fromBundle(extras), 0, true);
+        }
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        final Bundle extras = intent.getExtras();
+        if (extras == null) {
+            throw new IllegalArgumentException("Extras cannot be null");
+        }
+        final BackgroundTaskOptions bgOptions = new BackgroundTaskOptions(extras);
+        createNotificationChannel(bgOptions.getTaskTitle(), bgOptions.getTaskDesc()); // Necessary creating channel for API 26+
+        // Create the notification
+        final Notification notification = buildNotification(this, bgOptions);
+
+        if (bgOptions.isOngoing()) {
+            notification.flags |= Notification.FLAG_NO_CLEAR | Notification.FLAG_ONGOING_EVENT;
+        }
+
+        startForeground(SERVICE_NOTIFICATION_ID, notification);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void createNotificationChannel(@NonNull final String taskTitle, @NonNull final String taskDesc) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            final int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            final NotificationChannel channel = new NotificationChannel(CHANNEL_ID, taskTitle, importance);
+            channel.setDescription(taskDesc);
+            final NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        super.onTaskRemoved(rootIntent);
+
+        try {
+            WritableMap params = Arguments.createMap();
+            getReactContext().getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                    .emit("appKilled", params);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (currentBgOptions != null && currentBgOptions.isStopOnTerminate()) {
+            stopSelf();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        try {
+            WritableMap params = Arguments.createMap();
+            getReactContext().getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                    .emit("appKilled", params);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (currentBgOptions != null && currentBgOptions.isStopOnTerminate()) {
+            stopSelf();
+        }
+    }
+}
---
+++ lib/types/index.d.ts
@@ -0,0 +1,167 @@
+export const Priority: Readonly<{
+    MIN: number;
+    LOW: number;
+    DEFAULT: number;
+    HIGHT: number;
+    MAX: number;
+}>;
+export const Importance: Readonly<{
+    DEFAULT: number;
+    MAX: number;
+    HIGHT: number;
+    LOW: number;
+    MIN: number;
+    NONE: number;
+}>;
+export default backgroundServer;
+export type BackgroundTaskOptions = {
+    taskName: string;
+    taskTitle: string;
+    taskDesc: string;
+    taskIcon: {
+        name: string;
+        type: string;
+        package?: string;
+    };
+    color?: string | undefined;
+    linkingURI?: string | undefined;
+    priority?: number | undefined;
+    importance?: number | undefined;
+    ongoing?: boolean | undefined;
+    autoCancel?: boolean | undefined;
+    stopOnTerminate?: boolean | undefined;
+    progressBar?: {
+        max: number;
+        value: number;
+        indeterminate?: boolean | undefined;
+    } | undefined;
+};
+declare const backgroundServer: BackgroundServer;
+/**
+ * @typedef {{taskName: string,
+ *            taskTitle: string,
+ *            taskDesc: string,
+ *            taskIcon: {name: string, type: string, package?: string},
+ *            color?: string
+ *            linkingURI?: string,
+ *            priority?: number,
+ *            importance?: number,
+ *            ongoing?: boolean,
+ *            autoCancel?: boolean,
+ *            stopOnTerminate?: boolean,
+ *            progressBar?: {max: number, value: number, indeterminate?: boolean},
+ *            }} BackgroundTaskOptions
+ * @extends EventEmitter<'expiration',any>
+ */
+declare class BackgroundServer extends EventEmitter<"expiration", any> {
+    /** @private */
+    private _runnedTasks;
+    /** @private @type {(arg0?: any) => void} */
+    private _stopTask;
+    /** @private */
+    private _isRunning;
+    /** @private @type {BackgroundTaskOptions} */
+    private _currentOptions;
+    /**
+     * @private
+     */
+    private _addListeners;
+    /**
+     * **ANDROID ONLY**
+     *
+     * Updates the task notification.
+     *
+     * *On iOS this method will return immediately*
+     *
+     * @param {{taskTitle?: string,
+     *          taskDesc?: string,
+     *          taskIcon?: {name: string, type: string, package?: string},
+     *          color?: string,
+     *          linkingURI?: string,
+     *          priority?: number,
+     *          importance?: number,
+     *          ongoing?: boolean,
+     *          autoCancel?: boolean,
+     *          stopOnTerminate?: boolean,
+     *          progressBar?: {max: number, value: number, indeterminate?: boolean}}} taskData
+     */
+    updateNotification(taskData: {
+        taskTitle?: string;
+        taskDesc?: string;
+        taskIcon?: {
+            name: string;
+            type: string;
+            package?: string;
+        };
+        color?: string;
+        linkingURI?: string;
+        priority?: number;
+        importance?: number;
+        ongoing?: boolean;
+        autoCancel?: boolean;
+        stopOnTerminate?: boolean;
+        progressBar?: {
+            max: number;
+            value: number;
+            indeterminate?: boolean;
+        };
+    }): Promise<void>;
+    /**
+     * Returns if the current background task is running.
+     *
+     * It returns `true` if `start()` has been called and the task has not finished.
+     *
+     * It returns `false` if `stop()` has been called, **even if the task has not finished**.
+     */
+    isRunning(): boolean;
+    /**
+     * @template T
+     *
+     * @param {(taskData?: T) => Promise<void>} task
+     * @param {BackgroundTaskOptions & {parameters?: T}} options
+     * @returns {Promise<void>}
+     */
+    start<T>(task: (taskData?: T | undefined) => Promise<void>, options: {
+        taskName: string;
+        taskTitle: string;
+        taskDesc: string;
+        taskIcon: {
+            name: string;
+            type: string;
+            package?: string;
+        };
+        color?: string | undefined;
+        linkingURI?: string | undefined;
+        priority?: number | undefined;
+        importance?: number | undefined;
+        ongoing?: boolean | undefined;
+        autoCancel?: boolean | undefined;
+        stopOnTerminate?: boolean | undefined;
+        progressBar?: {
+            max: number;
+            value: number;
+            indeterminate?: boolean | undefined;
+        } | undefined;
+    } & {
+        parameters?: T | undefined;
+    }): Promise<void>;
+    /**
+     * @private
+     * @template T
+     * @param {(taskData?: T) => Promise<void>} task
+     * @param {T} [parameters]
+     */
+    private _generateTask;
+    /**
+     * @private
+     * @param {BackgroundTaskOptions} options
+     */
+    private _normalizeOptions;
+    /**
+     * Stops the background task.
+     *
+     * @returns {Promise<void>}
+     */
+    stop(): Promise<void>;
+}
+import EventEmitter from "eventemitter3";
