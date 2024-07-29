666c96e2138c94753694a3057ecd4d07fea9e5c6
max.wang <mstar0223@gmail.com>
2024-07-30 01:49:27 +0800

    iOS update: non-expire background task

--- ios/RNBackgroundActions.m
+++ ios/RNBackgroundActions.m
@@ -2,52 +2,64 @@
 #import "RNBackgroundActions.h"
 
 @implementation RNBackgroundActions {
-    UIBackgroundTaskIdentifier bgTask;
+  UIBackgroundTaskIdentifier bgTask;
 }
 
 RCT_EXPORT_MODULE()
 - (NSArray<NSString *> *)supportedEvents
 {
-    return @[@"expiration"];
+  return @[@"expiration"];
 }
 
 - (void) _start
 {
-    [self _stop];
-    bgTask = [[UIApplication sharedApplication] beginBackgroundTaskWithName:@"RNBackgroundActions" expirationHandler:^{
-        [self onExpiration];
-        [[UIApplication sharedApplication] endBackgroundTask: self->bgTask];
-        self->bgTask = UIBackgroundTaskInvalid;
-    }];
+  [self _stop];
+  self->bgTask = [self _startNew];
 }
 
 - (void) _stop
 {
-    if (bgTask != UIBackgroundTaskInvalid) {
-        [[UIApplication sharedApplication] endBackgroundTask:bgTask];
-        bgTask = UIBackgroundTaskInvalid;
-    }
+  if (bgTask != UIBackgroundTaskInvalid) {
+    [[UIApplication sharedApplication] endBackgroundTask:bgTask];
+    bgTask = UIBackgroundTaskInvalid;
+  }
+}
+
+- (UIBackgroundTaskIdentifier)_startNew {
+  UIBackgroundTaskIdentifier _bgTask = [[UIApplication sharedApplication] beginBackgroundTaskWithName:@"RNBackgroundActions" expirationHandler:^{
+    [self expirationHandler];
+  }];
+  
+  return _bgTask;
+}
+
+- (void) expirationHandler {
+  [self onExpiration];
+  UIBackgroundTaskIdentifier newBgTask = [self _startNew];
+  
+  [[UIApplication sharedApplication] endBackgroundTask: self->bgTask];
+  self->bgTask = newBgTask;
 }
 
 - (void)onExpiration
 {
-    [self sendEventWithName:@"expiration"
-                       body:@{}];
+  [self sendEventWithName:@"expiration"
+                     body:@{}];
 }
 
 RCT_EXPORT_METHOD(start:(NSDictionary *)options
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
 {
-    [self _start];
-    resolve(nil);
+  [self _start];
+  resolve(nil);
 }
 
 RCT_EXPORT_METHOD(stop:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
 {
-    [self _stop];
-    resolve(nil);
+  [self _stop];
+  resolve(nil);
 }
 
 @end
